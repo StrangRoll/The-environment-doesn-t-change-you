55c655c16616d9d113108df41b1b40199492e025
StrangRoll <strrranger1@gmail.com>
2021-11-16 02:52:35 +0300

    Added so-so smoge effect

--- Assets/Scripts/Spawner/Room.cs
+++ Assets/Scripts/Spawner/Room.cs
@@ -13,19 +13,31 @@ public class Room : MonoBehaviour
         int z = (int)Mathf.Round(transform.position.z / spawner.roomSize);
         if (x > 0)
             if (spawner.spawnedRooms[x - 1, z] != null)
+            {
                 transform.GetChild(0).gameObject.SetActive(false);
-
+                transform.GetChild(4).gameObject.SetActive(true);
+            }
+                
         if (x < spawner.roomCount)
             if (spawner.spawnedRooms[x + 1, z] != null)
+            {
                 transform.GetChild(1).gameObject.SetActive(false);
+                transform.GetChild(5).gameObject.SetActive(true);
+            }
 
         if (z < spawner.roomCount)
             if (spawner.spawnedRooms[x, z + 1] != null)
+            {
                 transform.GetChild(2).gameObject.SetActive(false);
+                transform.GetChild(6).gameObject.SetActive(true);
+            }
 
         if (z > 0)
             if (spawner.spawnedRooms[x, z - 1] != null)
+            {
                 transform.GetChild(3).gameObject.SetActive(false);
+                transform.GetChild(7).gameObject.SetActive(true);
+            }
     }
 
 }
---
+++ Assets/Scripts/Spawner/SpawnEnemy.cs
@@ -0,0 +1,9 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnEnemy : MonoBehaviour
+{
+    [SerializeField] private FirstPersonController player;
+
+}
