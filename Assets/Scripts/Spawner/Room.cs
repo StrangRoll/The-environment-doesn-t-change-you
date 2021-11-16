1fd1a887638a6da76eb49bf1e8d6362ea8749765
StrangRoll <strrranger1@gmail.com>
2021-11-16 17:36:21 +0300

    Add cool portal effect

--- Assets/Scripts/Spawner/Room.cs
+++ Assets/Scripts/Spawner/Room.cs
@@ -10,6 +10,7 @@ public class Room : MonoBehaviour
     {
         int x = (int)Mathf.Round(transform.position.x / spawner.roomSize);
         int z = (int)Mathf.Round(transform.position.z / spawner.roomSize);
+        if (transform.position.y < -50) return;
         if (x > 0)
             if (spawner.spawnedRooms[x - 1, z] != null)
             {
@@ -17,14 +18,14 @@ public class Room : MonoBehaviour
                 transform.GetChild(4).gameObject.SetActive(true);
             }
 
-        if (x < spawner.roomCount)
+        if (x < spawner.roomCount - 1)
             if (spawner.spawnedRooms[x + 1, z] != null)
             {
                 transform.GetChild(1).gameObject.SetActive(false);
                 transform.GetChild(5).gameObject.SetActive(true);
             }
 
-        if (z < spawner.roomCount)
+        if (z < spawner.roomCount - 1)
             if (spawner.spawnedRooms[x, z + 1] != null)
             {
                 transform.GetChild(2).gameObject.SetActive(false);
@@ -100,7 +101,8 @@ public class Room : MonoBehaviour
                 transform.GetChild(3).gameObject.SetActive(true);
                 transform.GetChild(7).gameObject.SetActive(false);
             }
-        
+        transform.GetChild(8).gameObject.SetActive(true);
+
     }
 
 }
