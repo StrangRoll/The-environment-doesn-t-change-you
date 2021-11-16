f951da9a7696b5153f482518f8ae75bac7f99c7e
StrangRoll <strrranger1@gmail.com>
2021-11-16 04:43:56 +0300

    Just some fix

--- Assets/Scripts/Spawner/Room.cs
+++ Assets/Scripts/Spawner/Room.cs
@@ -8,11 +8,6 @@ public class Room : MonoBehaviour
 
     void Start()
     {
-        OpenRoom();
-    }
-    public void OpenRoom()
-    {
-        //Debug.Log(Mathf.Round(transform.position.x / spawner.roomSize) - 1);
         int x = (int)Mathf.Round(transform.position.x / spawner.roomSize);
         int z = (int)Mathf.Round(transform.position.z / spawner.roomSize);
         if (x > 0)
@@ -21,7 +16,7 @@ public class Room : MonoBehaviour
                 transform.GetChild(0).gameObject.SetActive(false);
                 transform.GetChild(4).gameObject.SetActive(true);
             }
-                
+
         if (x < spawner.roomCount)
             if (spawner.spawnedRooms[x + 1, z] != null)
             {
@@ -43,11 +38,40 @@ public class Room : MonoBehaviour
                 transform.GetChild(7).gameObject.SetActive(true);
             }
     }
+    public void OpenRoom()
+    {
+        //Debug.Log(Mathf.Round(transform.position.x / spawner.roomSize) - 1);
+        int x = (int)Mathf.Round(transform.position.x / spawner.roomSize);
+        int z = (int)Mathf.Round(transform.position.z / spawner.roomSize);
+        if (x > 0)
+            if (spawner.spawnedRooms[x - 1, z] != null)
+            {
+                transform.GetChild(0).gameObject.SetActive(false);
+            }
+                
+        if (x < spawner.roomCount)
+            if (spawner.spawnedRooms[x + 1, z] != null)
+            {
+                transform.GetChild(1).gameObject.SetActive(false);
+            }
+
+        if (z < spawner.roomCount)
+            if (spawner.spawnedRooms[x, z + 1] != null)
+            {
+                transform.GetChild(2).gameObject.SetActive(false);
+            }
+
+        if (z > 0)
+            if (spawner.spawnedRooms[x, z - 1] != null)
+            {
+                transform.GetChild(3).gameObject.SetActive(false);
+            }
+    }
 
     public void CloseRoom()
     {
         //Debug.Log(Mathf.Round(transform.position.x / spawner.roomSize) - 1);
-        /*int x = (int)Mathf.Round(transform.position.x / spawner.roomSize);
+        int x = (int)Mathf.Round(transform.position.x / spawner.roomSize);
         int z = (int)Mathf.Round(transform.position.z / spawner.roomSize);
         if (x > 0)
             if (spawner.spawnedRooms[x - 1, z] != null)
@@ -76,7 +100,7 @@ public class Room : MonoBehaviour
                 transform.GetChild(3).gameObject.SetActive(true);
                 transform.GetChild(7).gameObject.SetActive(false);
             }
-        */
+        
     }
 
 }
